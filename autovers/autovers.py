d2d4b4bccaae56901e0cce77966c81711485d368
u1234x1234 <u1234x1234@gmail.com>
2018-11-02 02:31:12 +0300

    Add  pip, conda packages list saving

--- autovers/autovers.py
+++ autovers/autovers.py
@@ -2,17 +2,29 @@
 import glob
 import logging
 import os
-import tempfile
 import subprocess
+from contextlib import contextmanager
 
 import appdirs
 import git
 
 APPLICATION_NAME = 'autovers'
 EXTENSIONS = ['.py']
+PIP_LIST_PATH = 'pip_list.txt'
+CONDA_LIST_PATH = 'conda_list.txt'
 
 
-def commit():
+@contextmanager
+def TemporaryFile(filename, mode='w+'):
+    file = open(filename, mode=mode)
+    try:
+        yield file
+    finally:
+        file.close()
+        os.remove(filename)
+
+
+def commit(save_pip_state=True, save_conda_state=True):
     """Commit all files in current directory and return string with commit hash
     """
     logger = logging.getLogger(__name__)
@@ -38,12 +50,26 @@ def commit():
 
     repo.index.add(files)
 
-    with tempfile.NamedTemporaryFile(dir='./') as tmp_file:
-        out = subprocess.check_output(['pip', 'freeze', '--all'])
-        tmp_file.write(out)
-        tmp_file.flush()
-        print(tmp_file.name)
-        repo.index.add(tmp_file.name)
+    if save_pip_state:
+        with TemporaryFile(PIP_LIST_PATH) as tmp_file:
+            try:
+                out = subprocess.check_output(['pip', 'freeze', '--all'])
+                tmp_file.write(out.decode())
+                tmp_file.flush()
+                repo.index.add([os.path.join(working_dir, PIP_LIST_PATH)])
+            except Exception as e:
+                logger.warning('Error in executing pip freeze command: {}'.format(e))
+
+    if save_conda_state:
+        with TemporaryFile(CONDA_LIST_PATH) as tmp_file:
+            try:
+                out = subprocess.check_output(['conda', 'list', '--export'])
+                tmp_file.write(out.decode())
+                tmp_file.flush()
+                repo.index.add([os.path.join(working_dir, CONDA_LIST_PATH)])
+            except Exception as e:
+                logger.warning('Error in executing conda list command: {}'.format(e))
+
 
     if repo.head.is_valid():
         message = str(repo.head.commit.count() + 1)
@@ -52,5 +78,3 @@ def commit():
 
     repo.index.commit(message)
     return str(repo.head.commit)
-
-commit()
